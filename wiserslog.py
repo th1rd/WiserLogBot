939f3a34a30099db16097a48b28b15a24bca69a1
Zahidul Hasan <38650343+th1rd@users.noreply.github.com>
2025-04-10 09:21:56 +0600

    updated project for hosting

--- wiserslog.py
+++ wiserslog.py
@@ -1,30 +1,42 @@
 import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 from datetime import datetime
+from keep_alive import keep_alive
 
-load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
+google_credentials = os.getenv("GOOGLE_CREDENTIALS")
+
+# Convert the Google Credentials string into a Python dictionary
+creds_dict = json.loads(google_credentials)
+
 # Google Sheets Setup
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Create credentials using the dictionary from Replit secrets
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open("Wisers Log").sheet1  # Open first sheet
 
 # Discord Bot Setup
 intents = discord.Intents.default()
-intents.members = True  
+intents.members = True
 intents.message_content = True  # if bot reads messages
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+
 
 @bot.event
 async def on_message(message):
@@ -41,5 +53,8 @@ async def on_message(message):
             # Save to Google Sheet
             sheet.append_row([username, status, timestamp])
             print(f"Logged: {username} - {status} at {timestamp}")
+
+
+keep_alive()
 
 bot.run(TOKEN)
